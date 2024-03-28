fbb60200744fc35afe4876b60fc4206848254ef2
heldaysousa <helday.sousa@gmail.com>
2024-03-28 17:30:18 -0300

    ok

---
+++ lib/_system.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# 
+# system management
+
+#######################################
+# creates user
+# Arguments:
+#   None
+#######################################
+system_create_user() {
+  print_banner
+  printf "${WHITE} 💻 Agora, vamos criar o usuário para deploy innflow...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  useradd -m -p $(openssl passwd $deploy_password) -s /bin/bash -G sudo deployinnflow
+  usermod -aG sudo deployinnflow
+  mv "${PROJECT_ROOT}"/innflow.zip /home/deployinnflow/
+EOF
+
+  sleep 2
+}
+
+#######################################
+# unzip innflow
+# Arguments:
+#   None
+#######################################
+system_unzip_innflow() {
+  print_banner
+  printf "${WHITE} 💻 Fazendo unzip innflow...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - deployinnflow <<EOF
+  unzip innflow.zip
+EOF
+
+  sleep 2
+}
+
+#######################################
+# updates system
+# Arguments:
+#   None
+#######################################
+system_update() {
+  print_banner
+  printf "${WHITE} 💻 Vamos atualizar o sistema...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt -y update
+  sudo apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+EOF
+
+  sleep 2
+}
+
+#######################################
+# installs node
+# Arguments:
+#   None
+#######################################
+system_node_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando o Node.js...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  apt-get install -y nodejs
+  sleep 2
+  npm install -g npm@latest
+  sleep 2
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo apt-get update -y && sudo apt-get -y install postgresql
+  sleep 2
+  sudo timedatectl set-timezone America/Sao_Paulo
+  sleep 2
+  sudo -u postgres psql -c "ALTER USER postg
